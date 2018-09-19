3959fe662ec2dc2d2a177e495c024f5f42791b4e
Vitor Avian <Vitor@MacBook-Pro-de-Vitor.local>
2018-09-19 15:46:31 -0300

    Versão Entrada

--- main.cpp
+++ main.cpp
@@ -3,6 +3,25 @@
 using namespace std;
 
 int main(int argc, const char * argv[]) {
+    int R, C;
+    
+    cin>>R;
+    cin>>C;
+    
+    int tab[R][C];
+    
+    for(int i = 0; i<R; i++){
+        for(int j = 0;j<C; j++){
+            cin>>tab[i][j];
+        }
+    }
+    
+    for(int i = 0; i<R; i++){
+        for(int j = 0;j<C; j++){
+            cout<<tab[i][j]<<" ";
+        }
+        cout<<endl;
+    }
     
     return 0;
 }
