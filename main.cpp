67cf0bf20dfecf556276994587342231fe2a8969
Roque <rafael.roque@usp.br>
2018-09-19 20:11:59 -0300

    Movimentacao de pecas implementada. Falta parada.

--- main.cpp
+++ main.cpp
@@ -1,27 +1,129 @@
 #include <iostream>
+#include <tuple>
+#include <vector>
 
 using namespace std;
+vector <tuple <int, int, int>> pos;
+int R, C, total, dif;
+void backtracking(int **mat);
+
+int come(int i, int j, int pcome, int **mat){
+    int ret = 1;
+    if(i < R-1 && mat[i+1][j] == pcome ){
+        mat[i+1][j] =  mat[i][j];
+        mat[i][j] = 0;
+        backtracking(mat);
+        mat[i][j] = mat[i+1][j];
+        mat[i+1][j] = pcome;
+    }
+    if( i > 0 && mat[i-1][j] == pcome){
+        mat[i-1][j] =  mat[i][j];
+        mat[i][j] = 0;
+        backtracking(mat);
+        mat[i][j] = mat[i-1][j];
+        mat[i-1][j] = pcome;
+
+    }
+
+    if(j < C-1 && mat[i][j+1] == pcome ){
+        mat[i][j+1] =  mat[i][j];
+        mat[i][j] = 0;
+        backtracking(mat);
+        mat[i][j] = mat[i][j+1];
+        mat[i][j+1] = pcome;
+
+    }
+    if(j > 0 && mat[i][j-1] == pcome ){
+        mat[i][j-1] =  mat[i][j];
+        mat[i][j] = 0;
+        backtracking(mat);
+        mat[i][j] = mat[i][j-1];
+        mat[i][j-1] = pcome;
+    }
+    else 
+        ret = 0;
+    return ret;
+
+}
+
+void backtracking(int **mat){
+
+    int p=0;
+
+    int **aux = new int*[R]; //cria matriz auxiliar que contém a matriz alterada 
+    for(int i=0; i<R; i++)
+        aux[i]= new int[C];
+
+    for(int i=0; i<R; i++)
+        for(int j=0; j<C; j++)
+            aux[i][j] = mat[i][j];
+
+    for(int i=0; i<R; i++){
+        for(int j=0; j<C; j++){
+            switch(aux[i][j]){
+                case 1:
+                    if(come(i, j, 2, aux))
+                        p++;
+                    break;
+
+                case 2:
+                    if(come(i, j, 3, aux))
+                        p++;
+                    break;
+
+                case 3:
+                    if(come(i, j, 1, aux))
+                        p++;
+                    break;
+            }
+        }        
+    }
+    if(p == 1){
+        cout<< endl;
+        for(int i=0; i<R; i++){
+            for(int j=0; j<C; j++)
+                cout << mat[i][j] << " ";  
+            cout<< endl;
+        }
+    }
+    for(int i=0; i<R; i++)
+        delete[] aux[i];
+    delete[] aux;
+    
+
+}
+
+
 
 int main(int argc, const char * argv[]) {
-    int R, C;
     
     cin>>R;
     cin>>C;
+
+    int **tab;
+    tab = new int* [R];
+    for(int i=0; i<R; i++)
+        tab[i]= new int[C]; 
     
-    int tab[R][C];
     
     for(int i = 0; i<R; i++){
         for(int j = 0;j<C; j++){
             cin>>tab[i][j];
         }
     }
-    
-    for(int i = 0; i<R; i++){
+
+    backtracking(tab);
+
+ /*  for(int i = 0; i<R; i++){
         for(int j = 0;j<C; j++){
             cout<<tab[i][j]<<" ";
         }
         cout<<endl;
     }
+*/
+    for(int i=0; i<R; i++)
+        delete[] tab[i];
+    delete[] tab;
     
     return 0;
 }
